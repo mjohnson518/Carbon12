9f6b263a4bdb9334e41720a947cabffcde2f2aa7
Brian Rossetti <brossetti1@gmail.com>
2021-09-04 16:15:15 -0600

    cleaning up table a little bit - adding dark mode firefox

--- frontend/src/components/Footer/Copyright.jsx
+++ frontend/src/components/Footer/Copyright.jsx
@@ -1,8 +1,9 @@
 import { Text } from '@chakra-ui/layout'
 import * as React from 'react'
+import { PROJECT_NAME } from '../../constants'
 
 export const Copyright = (props) => (
   <Text fontSize="sm" {...props}>
-    &copy; {new Date().getFullYear()} Envelope, Inc. All rights reserved.
+    &copy; {new Date().getFullYear()} { PROJECT_NAME }, Inc. All rights reserved.
   </Text>
 )
