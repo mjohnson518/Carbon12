9891fd36adb2e8126f7346dacec1e84bedff83df
Spazzaroth <spazzaroth@gmail.com>
2021-09-06 15:31:55 -0500

    added another test case and exported the upload function.

--- test/test.js
+++ test/test.js
@@ -14,18 +14,24 @@ describe("Capture12", function () {
   before(async () => {
     const Capture12 = await ethers.getContractFactory("Capture12");
     [owner, addr1, addr2, ...addresses] = await ethers.getSigners();
-    console.log("owners", owner.address, addr1.address, addr2.address);
     capture12 = await Capture12.deploy();
     await capture12.deployed();
   });
-  it("should mint an nft to the owner", async function () {
-    console.log("owner address", owner.address);
+  it("should mint an nft to the owner address", async function () {
     const tx = await capture12.safeMint(
       owner.address,
       "ipfs://bafybeihkoviema7g3gxyt6la7vd5ho32ictqbilu3wnlo3rs7ewhnp7lly/"
     );
     const promise = await tx.wait();
-    console.log("PROMISE", promise.events[0].args.to);
+
     assert(promise.events[0].args.to === owner.address);
+  });
+  it("should mint an nft to addr1", async function () {
+    const tx = await capture12.safeMint(
+      addr1.address,
+      "ipfs://bafybeihkoviema7g3gxyt6la7vd5ho32ictqbilu3wnlo3rs7ewhnp7lly/"
+    );
+    const promise = await tx.wait();
+    assert(promise.events[0].args.to === addr1.address);
   });
 });
