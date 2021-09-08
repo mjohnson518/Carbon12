6a934b38f21ee78959b3014be195ee8d36ba75bb
Spazzaroth <spazzaroth@gmail.com>
2021-09-08 16:39:00 -0500

    todays work

--- frontend/server.js
+++ frontend/server.js
@@ -1,60 +1,64 @@
-require("dotenv").config();
+require('dotenv').config();
 const express = require('express');
 const https = require('https');
 const http = require('http');
 const cors = require('cors');
 const axios = require('axios').default;
+const create = require('ipfs-http-client');
 
-const PORT = 3001
+const PORT = 3001;
 const app = express();
 
 app.use(cors());
 
 const options = {};
 
-http.createServer(app).listen(80)
-https.createServer(options, app).listen(443)
+http.createServer(app).listen(80);
+https.createServer(options, app).listen(443);
 
-app.get('/typeform', async(req, res) => {
-  await axios.get("https://api.typeform.com/forms/t4Wsz3R9/responses", {
-    headers: {
-      Authorization: `Bearer C8vuMvr9qb3yBL2LaLr8ByGkNwuU382V8NCnr8w6G6UB`,
-      crossdomain: true,
-      Accept: "application/json",
-      "Content-Type": "application/json"
-      }
-    }
-  ).then((resp) => {
-    return res.status(res.statusCode)
-              .json(resp.data)
-  })
-  .catch((error) => {
-    return res.status(res.statusCode)
-              .send({success : false , message: error.message})
-  })
-})
+app.get('/typeform', async (req, res) => {
+  await axios
+    .get('https://api.typeform.com/forms/t4Wsz3R9/responses', {
+      headers: {
+        Authorization: `Bearer C8vuMvr9qb3yBL2LaLr8ByGkNwuU382V8NCnr8w6G6UB`,
+        crossdomain: true,
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+    })
+    .then(resp => {
+      return res.status(res.statusCode).json(resp.data);
+    })
+    .catch(error => {
+      return res
+        .status(res.statusCode)
+        .json({ success: false, message: error.message });
+    });
+});
 
-app.post('/create-qr-code', async(req, res) => {
+app.post('/upload-to-ipfs', async (req, res) => {
   // request for create-qr-code
-  // await axios.get("https://api.typeform.com/forms/t4Wsz3R9/responses", {
-  //   headers: {
-  //     Authorization: `Bearer C8vuMvr9qb3yBL2LaLr8ByGkNwuU382V8NCnr8w6G6UB`,
-  //     crossdomain: true,
-  //     Accept: "application/json",
-  //     "Content-Type": "application/json"
-  //     }
-  //   }
-  // ).then((resp) => {
-  //   return res.status(res.statusCode)
-  //             .json(resp.data)
-  // })
-  // .catch((error) => {
-  //   return res.status(res.statusCode)
-  //             .send({success : false , message: error.message})
-  // })
-})
+  console.log('server ping');
+  async function uploadToIPFS(questionaire) {
+    const ipfs = create();
+    await ipfs
+      .add(ipfsData)
+      .then(resp => {
+        return res.status(res.statusCode).json(resp.data);
+      })
+      .catch(err => {
+        return res
+          .status(res.statusCode)
+          .json({ success: false, message: error.message });
+      });
 
-app.get('/read-qr-code', async(req, res) => {
+    // manipulate data post questionaire upload to ipfs
+
+    // store questionaire in localstorage
+  }
+});
+
+app.get('/read-qr-code', async (req, res) => {
   // request for read-qr-code
   // await axios.get("https://api.typeform.com/forms/t4Wsz3R9/responses", {
   //   headers: {
@@ -72,8 +76,8 @@ app.get('/read-qr-code', async(req, res) => {
   //   return res.status(res.statusCode)
   //             .send({success : false , message: error.message})
   // })
-})
+});
 
 app.listen(PORT, () => {
-  console.log(`Example app listening at http://localhost:${PORT}`)
-})
+  console.log(`Example app listening at http://localhost:${PORT}`);
+});
--- frontend/src/components/Table/TableContent.jsx
+++ frontend/src/components/Table/TableContent.jsx
@@ -1,26 +1,53 @@
-import { Button, Table, Tbody, Td, Th, Thead, Tr, useColorModeValue as mode } from '@chakra-ui/react';
+import {
+  Button,
+  Table,
+  Tbody,
+  Td,
+  Th,
+  Thead,
+  Tr,
+  useColorModeValue as mode,
+} from '@chakra-ui/react';
+import axios from 'axios';
 import { create } from 'ipfs-http-client';
 import * as React from 'react';
 
-import { columns, getFormData, handleTypeFormField } from './typeform';
+import {
+  columns,
+  getFormData,
+  handleTypeFormField,
+  getFormByID,
+  parseAnswers,
+} from './typeform';
 
-export const TableContent = (props) => {
-  const client = create();
+export const TableContent = props => {
   const forms = getFormData() || [];
-  const formAnswers = forms.map((form) => form.answers);
+  const formAnswers = forms.map(form => form.answers);
 
   async function uploadToIPFS(questionaire) {
     // manipulate questionaire data
-
+    const answersObj = parseAnswers(questionaire);
     // check if questionaire has already been minted
-
     // create ipfs link
-    const carbonForm = await client.dag.put(questionaire);
-
+    const ipfsData = JSON.stringify({
+      path: '/',
+      content: answersObj,
+      mode: 'string',
+      mtime: Date.now(),
+    });
+    let ipfsURI;
+    axios
+      .post('/upload-to-ipfs', ipfsData)
+      .then(res => {
+        //ipfsURI = res.data;
+        console.log(res);
+      })
+      .catch(err => {
+        console.error(err);
+      });
     // manipulate data post questionaire upload to ipfs
 
     // store questionaire in localstorage
-
   }
 
   return (
@@ -36,29 +63,32 @@ export const TableContent = (props) => {
         </Tr>
       </Thead>
       <Tbody>
-        {
-          formAnswers.map((answers, index) => (
-            <Tr key={index}>
-              { columns.map((column, index) => {
-                  const item = answers.find((col) => col.field.ref === column.accessor);
-                  const cell = item ? handleTypeFormField(item) : 'N/A';
+        {formAnswers.map((answers, index) => (
+          <Tr key={index}>
+            {columns.map((column, index) => {
+              const item = answers.find(
+                col => col.field.ref === column.accessor
+              );
+              const cell = item ? handleTypeFormField(item) : 'N/A';
 
-                  return (
-                    <Td whiteSpace="nowrap" key={index}>
-                      {cell}
-                    </Td>
-                  )
-                })
-              }
-              <Td textAlign="right">
-                <Button size="sm" colorScheme="teal" onClick={ async () => uploadToIPFS(answers) }>
-                  Mint
-                </Button>
-              </Td>
-            </Tr>
-          ))
-        }
+              return (
+                <Td whiteSpace="nowrap" key={index}>
+                  {cell}
+                </Td>
+              );
+            })}
+            <Td textAlign="right">
+              <Button
+                size="sm"
+                colorScheme="teal"
+                onClick={async () => uploadToIPFS(answers)}
+              >
+                Mint
+              </Button>
+            </Td>
+          </Tr>
+        ))}
       </Tbody>
     </Table>
-  )
-}
+  );
+};
